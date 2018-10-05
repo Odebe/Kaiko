bc292656d2f910245bc27fd72a317cc2671d8a76
Odebe <derpiranha@gmail.com>
2018-10-05 22:25:52 +0300

    fix policies

--- app/policies/admin/application_policy.rb
+++ app/policies/admin/application_policy.rb
@@ -2,27 +2,31 @@ module Admin
   class ApplicationPolicy < ::ApplicationPolicy
 
     def index?
-      user.operator? || user.admin? 
+      user.operator? || user.admin?
+    end
+
+    def new?
+      user.operator? || user.admin?
     end
 
     def show?
-      user.operator? || user.admin? 
+      user.operator? || user.admin?
     end
 
     def create?
-      user.operator? || user.admin? 
+      user.operator? || user.admin?
     end
 
     def edit?
-      user.operator? || user.admin? 
+      user.operator? || user.admin?
     end
 
     def update?
-      user.operator? || user.admin? 
+      user.operator? || user.admin?
     end
 
-    def delete?
-      user.operator? || user.admin? 
+    def destroy?
+      user.operator? || user.admin?
     end
 
   end
